4493634d0ff17ab6d6205641c668e9cbe0495370
mstat777 <statevd@gmail.com>
2023-11-05 00:29:23 +0100

    14th commit: bug fixed

--- client/src/Components/Containers/Burger/Index.jsx
+++ client/src/Components/Containers/Burger/Index.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 function Burger() {
     const navigate = useNavigate();
@@ -11,10 +12,19 @@ function Burger() {
     const { userInfo } = useSelector(state => state.user);
 
     const [burgerOpen, setBurgerOpen] = useState(false);
+    const [isHovering, setIsHovering] = useState(false);
 
     const toggleBurgerOrLogin = () => {
         !userInfo.isLogged ? navigate("/user/signin") : setBurgerOpen(!burgerOpen);
     }
+
+    const handleMouseOver = () => {
+        setIsHovering(true);
+    };
+    
+    const handleMouseOut = () => {
+        setIsHovering(false);
+    };
 
     return (
         <div className={`${styles.burger_menu} ${burgerOpen ? styles.show_burger_menu : styles.hide_burger_menu }`}>
@@ -22,20 +32,28 @@ function Burger() {
             <div className={styles.burger_items_ctn}>
                 {userInfo.isLogged && (
                         <>
+                            <button onClick={toggleBurgerOrLogin} className={styles.burger_cross_btn}>
+                                <FontAwesomeIcon icon={faXmark} className={styles.burger_cross}/>
+                            </button>
+
                             <NavLink to={"/"} className={styles.burger_item}>Info personnelles</NavLink>
                             <NavLink to={"/"} className={styles.burger_item}>Mes réservations</NavLink>
-                            <NavLink to={"/user/signout"} className={styles.burger_item} onClick={() => toggleBurgerOrLogin()}>déconnexion</NavLink>    
+                            
+                            <NavLink to={"/user/signout"} className={styles.burger_item_signout} onClick={() => toggleBurgerOrLogin()}>déconnexion</NavLink>
                         </>
                     )}
             </div>
             
-            <button 
-            onClick={() => toggleBurgerOrLogin()} 
-            className={styles.burger_icon} 
-            title="Espace utilisateur" 
-            data-content={!userInfo.isLogged ? "connexion" : ""}>
-                <FontAwesomeIcon icon={faUser} className={styles.burger_icon_fawesome}/>
-            </button>
+     
+                <button 
+                onClick={toggleBurgerOrLogin} 
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseOut}
+                className={`${styles.burger_icon} ${burgerOpen ? styles.hide : styles.show}`} 
+                title="Espace utilisateur">
+                    {(isHovering && !userInfo.isLogged) && <span>CONNEXION&nbsp;</span>}
+                    <FontAwesomeIcon icon={faUser} className={styles.burger_icon_fawesome}/>
+                </button>
 
         </div>
     )
