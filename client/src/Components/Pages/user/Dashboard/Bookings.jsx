a0aa70ac233f8a249e1d26f930ccf570b8898f2b
mstat777 <statevd@gmail.com>
2023-11-07 15:12:27 +0100

    18th commit: user DB forms added

--- client/src/Components/Pages/user/Dashboard/Bookings.jsx
+++ client/src/Components/Pages/user/Dashboard/Bookings.jsx
@@ -1,4 +1,5 @@
 import styles from './dashboard.module.css';
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
@@ -9,9 +10,47 @@ function UserDashboardBookings(){
 
     const { userInfo } = useSelector(state => state.user);
 
+    const [ userBookings, setUserBookings] = useState([]);
+
+    useEffect(() => {
+        // on récupère les données des réservations effectuées par l'utilisateur :
+        async function fetchBookings() {
+            try {
+                const dataBookings = await (await fetch(`/api/v.0.1/user/bookings/${userInfo.userID}`)).json();
+                setUserBookings(dataBookings.datas);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchBookings();
+    }, []);
+
     return(
         <main className={styles.user_db_main}>
             <h2>mes réservations</h2>
+
+            <table className={styles.booking_table}>
+                <thead>
+                    <tr> 
+                        <th>N&deg;</th> 
+                        <th>date</th>
+                        <th>destination</th>
+                        <th>modifier</th>
+                    </tr>
+                </thead>
+                <tbody>  
+                    { userBookings.map((booking, index) => 
+                        <tr key={index}>
+                            <td>{index+1}</td>
+                            <td>{booking.date_created.slice(0, booking.date_created.indexOf('T'))}</td>
+                            <td>{booking.destination}</td>
+                            <td>
+                                <button>modifier</button>
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
 
         </main>
     )
--- client/src/Components/Pages/user/Dashboard/Infos.jsx
+++ client/src/Components/Pages/user/Dashboard/Infos.jsx
@@ -1,4 +1,5 @@
 import styles from './dashboard.module.css';
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
@@ -6,13 +7,113 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
 
 function UserDashboardInfos(){
-
+    // pour récuperer l'ID de l'utilisateur
     const { userInfo } = useSelector(state => state.user);
+
+    const [formData, setFormData] = useState({
+        lastName: "",
+        firstName: "",
+        email: "",
+        tel: "",
+        addresse: "",
+        birthDate: "",
+        occupation: ""
+    });
+
+    /*const [lastName, setLastName] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [email, setEmail] = useState("");
+    const [tel, setTel] = useState("");
+    const [addresse, setAddresse] = useState("");
+    const [birthDate, setBirthDate] = useState("");
+    const [occupation, setOccupation] = useState("");*/
+
+    const [msg, setMsg] = useState(null);
+
+    useEffect(() => {
+        // on récupère les données de l'utilisateur pour 'automatiquement' remplir le formulaire lors du chargement de la page :
+        async function fetchUser() {
+            try {
+                //console.log("userInfo.userID = "+userInfo.userID);
+                const dataUser = await (await fetch(`/api/v.0.1/user/${userInfo.userID}`)).json();
+                setFormData({
+                    lastName: dataUser.datas[0].last_name,
+                    firstName: dataUser.datas[0].first_name,
+                    email: dataUser.datas[0].email,
+                    tel: dataUser.datas[0].tel,
+                    addresse: dataUser.datas[0].addresse,
+                    birthDate: dataUser.datas[0].birth_date,
+                    occupation: dataUser.datas[0].occupation
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchUser();
+    },[])
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+    }
 
     return(
         <main className={styles.user_db_main}>
             <h2>nes informations personnelles</h2>
 
+            <form onSubmit={handleSubmit} className={styles.user_db_form}>
+                <label>
+                    <span>Votre nom : </span>
+                    <input type="text" 
+                        name="lastName" 
+                        value={formData.lastName}
+                        onChange={handleChange}/>
+                </label>
+                <label>
+                    <span>Votre prénom : </span>
+                    <input type="text" 
+                        name="firstName" 
+                        value={formData.firstName}
+                        onChange={handleChange}/>
+                </label>
+                <label>
+                    <span>Votre prénom : </span>
+                    <span className={styles.email}>{formData.email}</span>
+                </label>
+                <label>
+                    <span>Votre numéro de téléphone : </span>
+                    <input type="tel" 
+                        name="tel" 
+                        value={formData.tel}
+                        onChange={handleChange}/>
+                </label>
+                <label>
+                    <span>Votre adresse : </span>
+                    <textarea type="text" 
+                        name="addresse" 
+                        value={formData.addresse}
+                        onChange={handleChange}/>
+                </label>
+                <label>
+                    <span>Votre date de naissance : </span>
+                    <input type="date" 
+                        name="birthDate" 
+                        min="1923-01-01"
+                        value={formData.birthDate}
+                        onChange={handleChange}/>
+                </label>
+                <label>
+                    <span>Votre métier : </span>
+                    <input type="text" 
+                        name="occupation" 
+                        value={formData.occupation}
+                        onChange={handleChange}/>
+                </label>
+                <button type="submit">modifier</button>
+            </form>
         </main>
     )
 }
